e33747869132c10d1d9050f1c26292d8f81b4acb
angelestarita <101217260+xAd4@users.noreply.github.com>
2024-09-09 19:12:55 -0400

    Optimization - Last commit of the day 9/9/2024

--- appointments/templatetags/departments.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from departments.models  import Deparment
-
-register = template.Library()
-
-@register.simple_tag
-def get_department():
-    department = Deparment.objects.all()
-    return department
--- appointments/templatetags/doctors.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from doctors.models import Doctor
-
-register = template.Library()
-
-@register.simple_tag
-def get_doctor():
-    doctor = Doctor.objects.all()
-    return doctor
